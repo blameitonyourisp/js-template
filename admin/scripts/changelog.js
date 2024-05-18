7fd78c34cc74f65467a4b0a968924328b967e003
James Reid <jamesreid3806@gmail.com>
2024-05-18 13:41:49 +0100

    int^ Fix private repo label bug
    WHAT: Fix bug preventing update label script from working on private
    repos.

--- admin/scripts/changelog.js
+++ admin/scripts/changelog.js
@@ -151,6 +151,12 @@ const defaults = /** @type {Object.<string,CliOption>} */ ({
         value: "CHANGELOG.md",
         description: "Relative path to changelog from repo package.json file."
     },
+    token: {
+        name: "token",
+        aliases: ["t"],
+        value: process.env.GITHUB_ACCESS_TOKEN || "",
+        description: "GitHub fine-grained access token for updating labels."
+    },
     lastTag: {
         name: "last-tag",
         aliases: ["t"],
@@ -190,7 +196,7 @@ const cli = /** @type {ChangelogCliOptions} */
     (parseCliArguments("docs:changelog", defaults))
 
 // Check that git repository exists on github.
-await checkRemote(cli.repoOwner, cli.repoName)
+await checkRemote(cli.repoOwner, cli.repoName, cli.token)
 
 // Object for markdown and frontmatter props to be rendered to as final step.
 const changelog = /** @type {{markdown:string, frontmatter:Frontmatter}} */ ({})
